4fa7436e125ad9689c6eb875347c15d8fe32d7fe
Sneha_0127 <snehamk27112005@gmail.com>
2025-04-29 09:00:14 +0530

    modify

--- Employee.java
+++ Employee.java
@@ -6,23 +6,26 @@ public class Employee {
     int id;
     double salary;
     String address;
+    int age;
 
-    public Employee(String name, int id, double salary,String address){
+    public Employee(String name, int id, double salary,String address,int age){
         this.name=name;
         this.id=id;
         this.salary=salary;
         this.address=address;
+        this.age=age;
     }
     public void display(){
         System.out.println("Name : "+name);
         System.out.println("ID "+id);
         System.out.println("Salary : "+salary);
         System.out.println("address : "+address);
+        System.out.println("Age : "+age);
 
     }
 
     public static void main(String[] args){
-        Employee e = new Employee("Shubh",043,1200000.00,"banglore");
+        Employee e = new Employee("Shubh",043,1200000.00,"banglore",20);
         e.display();
 
     }
